07181aacefe46acd48fe0c6b661061a399fef30b
anu-ship-it <anup17508@gmail.com>
2026-06-02 03:22:35 +0530

    finvir logic is builded

--- backend/db/__init__.py
+++ backend/db/__init__.py
@@ -1,5 +1,5 @@
 # backend/bd/__init__.py
-from backend.db.database import Base, enginer, SessionLocal, get_db, init_db
+from backend.db.database import Base, engine, SessionLocal, get_db, init_db
 from backend.db.models import ScanResult, StockSignal
 
 __all__ = [
