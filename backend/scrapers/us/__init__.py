b70947b558f711909e04fc31322b89d112577cb7
anu-ship-it <anup17508@gmail.com>
2026-06-02 03:10:58 +0530

    Renaming of few files

--- backend/scrapers/us/__init__.py
+++ backend/scrapers/us/__init__.py
@@ -1,8 +1,8 @@
 # backend/scrapers/us/__init__.py
-from backend.scrapers.us.finiver_scraper import fetch_finviz_penny_stocks
+from backend.scrapers.us.finvir_scraper import fetch_finviz_penny_stocks
 from backend.scrapers.us.yahoo_scraper import enrich_tickers
 from backend.scrapers.us.reddit_scraper import fetch_reddit_signals
-from backend.scrapers.us.stockwits_scraper import fetch_bulk_stocktwits
+from backend.scrapers.us.stocktwits_scraper import fetch_bulk_stocktwits
 from backend.scrapers.us.sec_scraper import fetch_insider_buys
 
 __all__ = [
